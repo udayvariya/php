54d95e4d77eada73e92344a4ae501c4b82dbf70d
udayvariya <udayvariya302@gmail.com>
2024-10-07 17:50:24 +0530

    all

--- forgetpassword.php
+++ forgetpassword.php
@@ -14,40 +14,40 @@ session_start();
     function sendmail($email,$reset_token){
         echo $email;
         echo $reset_token;
-        require('/project/phpmaier/PHPMailer.php');
-        require('/project/phpmaier/SMTP.php');
-        require('/project/phpmaier/Exception.php');
+        // require('/project/phpmaier/PHPMailer.php');
+        // require('/project/phpmaier/SMTP.php');
+        // require('/project/phpmaier/Exception.php');
 
-        $mail = new PHPMailer(true);
+        // $mail = new PHPMailer(true);
 
-        try {
-            //Server settings
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'udayvariya302@gmail.com';                     //SMTP username
-            $mail->Password   = 'Uday@2757';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        // try {
+        //     //Server settings
+        //     $mail->isSMTP();                                            //Send using SMTP
+        //     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        //     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        //     $mail->Username   = 'udayvariya302@gmail.com';                     //SMTP username
+        //     $mail->Password   = 'Uday@2757';                               //SMTP password
+        //     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        //     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         
-            //Recipients
-            $mail->setFrom('udayvariya302@gmail.com', 'UDAY PASS');
-            $mail->addAddress($email);     //Add a recipient
+        //     //Recipients
+        //     $mail->setFrom('udayvariya302@gmail.com', 'UDAY PASS');
+        //     $mail->addAddress($email);     //Add a recipient
         
             
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Password Reset Link from uday';
-            $mail->Body    = "we got request the reset password link <br>
-            click the link below : <br>
-            <a href='http://localhost/php/updatepassword.php?email=$email&reset_token=$reset_token'>Reset Password</a>";
+        //     //Content
+        //     $mail->isHTML(true);                                  //Set email format to HTML
+        //     $mail->Subject = 'Password Reset Link from uday';
+        //     $mail->Body    = "we got request the reset password link <br>
+        //     click the link below : <br>
+        //     <a href='http://localhost/php/updatepassword.php?email=$email&reset_token=$reset_token'>Reset Password</a>";
         
-            $mail->send();
-            return true;
-        }
-        catch (Exception $e) {
-            return false;
-        }
+        //     $mail->send();
+        //     return true;
+        // }
+        // catch (Exception $e) {
+        //     return false;
+        // }
 
     }
     if(isset($_POST['send-email-link'])){
